e82d58dfaff7302cf52bf6404b530493c97cd5aa
Cristhian Paolo <cristh715@gmail.com>
2023-08-25 16:38:09 -0500

    Tipos de datos en php

--- PHP/Sintaxis.php
+++ PHP/Sintaxis.php
@@ -1,6 +1,18 @@
 <?php //Incrustando código php 
-
-    echo "Hola PHP"; //Imprimir en consola
-    $variable = "\n" . "Hola"; // La instrucción "." -> Concatenar
-    echo $variable;
+    //Imprimir en consola
+    $saludo = "Hola mundo"; //Variables definidas con $
+    $lenguaje = "en PHP"; //Imprimir en consola
+    echo $saludo . ", " . $lenguaje; //Concatenar con "."
+    
+    echo "\n";
+    //Tipos de datos escalares
+    $integer = 10; //Integer
+    $float = 2.5;
+    $string = "Palabra";
+    $boolean = true;
+    echo $integer . " - ". gettype($integer) . "\n";
+    echo $float . " - ". gettype($float) . "\n";
+    echo $string . " - ". gettype($string) . "\n";
+    echo $boolean . " - ". gettype($boolean) . "\n";
+    //Prueba
 ?> 
