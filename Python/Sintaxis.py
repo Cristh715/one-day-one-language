92775373ba3c9db38aebb3f3c43794daba72a90a
Cristhian Paolo <cristh715@gmail.com>
2023-09-07 10:55:33 -0500

    Condicionales en python

--- Python/Sintaxis.py
+++ Python/Sintaxis.py
@@ -1,1 +1,11 @@
-print('Hola mundo')
+# Hola mundo
+saludo = 'Ho' + 'la' #Concatenando string
+print(saludo, ' mundo') #Concatenando en salida de dato, usando print.
+
+# Declaración e ingreso de datos
+i, j = 0, 4 #Asignación multiple de variables
+num = input ("Digite el valor de num: \n -> ")
+
+# Condicionales
+if i == 0 or j==5: #Operadores booleanos and - or
+    print ("Hola, el valor de num es ", num)
